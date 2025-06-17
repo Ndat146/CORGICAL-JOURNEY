0525f7319d23354fb33445c8a270250132b4bd2f
Ndat146 <tranggg146987@gmail.com>
2025-06-17 22:34:39 +0700

    Update
    Spawn map, UI select level

---
+++ Assets/scripts/Spawn Map/MapData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "MapData", menuName = "Map/MapData")]
+public class MapData : ScriptableObject
+{
+    [System.Serializable]
+    public struct BlockInfo
+    {
+        public BlockType blockType; 
+        public Vector3 position;    
+    }
+
+    public BlockInfo[] blocks; 
+}
---
+++ Assets/scripts/Spawn Map/MapSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class MapSpawner : MonoBehaviour
+{
+    public MapData mapData;  
+    public GameObject soilPrefab;      
+    public GameObject soilWithTreePrefab; 
+
+    void Start()
+    {
+        SpawnMap();
+    }
+
+    void SpawnMap()
+    {
+        
+        foreach (var block in mapData.blocks)
+        {
+            GameObject blockPrefab = null;
+
+            // Chọn prefab phù hợp dựa trên loại block
+            switch (block.blockType)
+            {
+                case BlockType.Grass:
+                    blockPrefab = soilPrefab;
+                    break;
+                case BlockType.GrassWithTree:
+                    blockPrefab = soilWithTreePrefab;
+                    break;
+            }
+
+            
+            if (blockPrefab != null)
+            {
+                Instantiate(blockPrefab, block.position, Quaternion.identity);
+            }
+        }
+    }
+}
